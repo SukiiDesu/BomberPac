4f003c1aba29013ec16da633e4eda2f2d2d20066
João Pedro Barros Silva Azevedo <joaobsaprofissional@gmail.com>
2025-06-09 03:13:35 -0300

    PREENCHE_MATRIZ()
    Agora a função preenche matriz funciona para todo o frame adequadamente. O preenchimento é coluna a coluna

--- macros/RENDERIZA.s
+++ macros/RENDERIZA.s
@@ -63,14 +63,96 @@
 ########################################################
 #                     MACROS
 ########################################################
-	# Como que "acompanha" qual etapa da animacao deve ser renderizada ???????? Deve ser feito em ATUALIZA_FRAME????
-	# "Conferir passagem dos registradores"
-	# Para todas as imagens necessarias:
-		# Carrega endereco da imagem em um registrador
-		# Desenha_imagem(%reg) # Chama Desenha_imagem com o registrador que guarda o endereco correto para desenho do objeto
+
+# Atualiza as cores do frame e itera os enderecos do frame e da imagem
+# %reg1 == Endereco atual do frame
+# %reg2 == Endereco atual da imagem
+.macro PREENCHE_WORD(%reg1, %reg2)
+	lw t4,0(%reg2)		# t4 = Conteudo do Endereco atual da imagem
+	sw t4,0(%reg1)		# 4 pixels a partir do Endereco atual do frame mudam de cor
+	addi %reg1,%reg1,4	# Atualiza Endereco atual do frame em 4 bytes
+	#addi %reg2,%reg2,4	# Atualiza Endereco atual da imagem em 4 bytes
+.end_macro 
+
+# Atualiza as cores do frame e itera os enderecos do frame e da imagem
+# %reg1 == Endereco atual do frame
+# %reg2 == Endereco atual da imagem
+.macro PREENCHE_BYTE(%reg1,%reg2)
+	#lb t4,0(%reg2)		# t4 = 1 pixel cor
+	sb t4,0(%reg1)		# 4 pixels a partir do Endereco atual do frame mudam de cor
+	addi %reg1,%reg1,1	# Atualiza Endereco atual do frame em 1 byte
+	#addi %reg2,%reg2,1	# Atualiza Endereco atual da imagem em 1 byte
+.end_macro
+
+# Recebe o endereco mais a esquerda e acima da matriz : [0,0]
+.macro PREENCHE_MATRIZ(%reg1, %reg2)
+	li s5,16	# Tamanho de linhas e colunas
+	li s6,320	# Pixels por linha
+	
+	li t1,0		# linha = 0
+LOOP_LINHA:
+	beq t1,s5,FIM_PREENCHE_MATRIZ	# Extrapolou a linha
+	li t3,0				# coluna = 0
+LOOP_COLUNA:
+	beq t3,s5,SOMA_LINHA		# Enquanto coluna < 16
+	PREENCHE_WORD(%reg1,%reg2)	
+	#PREENCHE_BYTE(%reg1`,%reg2)
+	addi t3,t3,4			# coluna++
+	j LOOP_COLUNA
+
+SOMA_LINHA:
+	addi t1,t1,1	# linha++
+	#Ainda nao extrapolou a linha
+	sub %reg1,%reg1,s5
+	add %reg1,%reg1,s6
+	j LOOP_LINHA
+
+FIM_PREENCHE_MATRIZ:
+.end_macro 
 
 
+.data
+	.eqv ENDERECO_INICIAL_FRAME_0 0xFF000000
+	.eqv ENDERECO_FIM_FRAME_0 0xFF012C00
+	.eqv NUM_MAGICO_1 5440
+	.eqv NUM_MAGICO_2 76160
+	VERDE_WORD: .word 0x10101010
+	VERDE_BYTE: .byte 0x10
+	VERMELHO_WORD: .word 0x07070707
+	VERMELHO_BYTE: .byte 0x07
+.text
+	li t0,ENDERECO_INICIAL_FRAME_0
+	li t5,0x11800
+	add t5,t0,t5	# t5 = Endereco inicial da ultima matriz da linha
+	
+	li t6,0x130
+	add t6,t0,t6	# t6 = Endereco inicial da ultima matriz do frame
+	
+	la t2, VERDE_WORD
+LOOP_FRAME:
+	bgt t0,t6,EXIT
+LOOP_LINHA_FRAME:
+	PREENCHE_MATRIZ(t0,t2)
+	mul s0,s0,s1
+	bgt t0,t5,PROXIMA_COLUNA_FRAME
+	j LOOP_LINHA_FRAME
 
+PROXIMA_COLUNA_FRAME:
+	# Corrige posicao do endereco da proxima matriz
+	li t1,76800	# t1 = Total de pixels da posicao atual ate a primeira linha do frame
+	sub t0,t0,t1	# Volte para o inicio da coluna que estamos
+	li t1,16	# t1 = colunas entre uma matriz e outra
+	add t0,t0,t1	# Va para a proxima matriz
+	
+	li t5,0x11800
+	add t5,t0,t5	# t5 = Endereco inicial da ultima matriz da coluna
+	j LOOP_FRAME
+
+EXIT:
+	#lw t2,VERMELHO_WORD
+	#sw t2,0(t0)
+	li a7,10
+	ecall
 # Renderiza usando TIME() como separador: recebe o endereco base em um reg1 e o tempo base em reg2
 #.macro RENDERIZA_TIME(%reg1, %reg2)
 #.end_macro
