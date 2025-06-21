f38b9d6ea808ddafd74b011b6c3b4762351bad21
João Pedro Barros Silva Azevedo <joaobsaprofissional@gmail.com>
2025-06-21 15:39:53 -0300

    Melhoria na musica
    Som está tocando executado melhor. Retiro a gambiarra anterior.

--- main.s
+++ main.s
@@ -6,13 +6,13 @@ CONFIGURA_FASE_1:
 ##########################################################
 # Renderiza os frames estaticamente (por completo) #######
 ##########################################################
-	#.include "RENDERIZA_FRAME_0_FASE_1.s"
-	#.include "RENDERIZA_FRAME_1_FASE_1.s"
+	.include "RENDERIZA_FRAME_0_FASE_1.s"
+	.include "RENDERIZA_FRAME_1_FASE_1.s"
 
 ################################################
 ###### Inicializa todos os scores ##############
 ################################################
-	#.include "INICIALIZA_SCORES.s"
+	.include "INICIALIZA_SCORES.s"
 
 ##############################################################
 # Inicializa todos os TEMPO_INICIAL das funcoes periodicas ###
@@ -26,12 +26,13 @@ CONFIGURA_FASE_1:
 	add a0, a0, t0		# "Atrasa" TEMPO_ATUAL
 											
 	# Inicializa TEMPO_INICIAL_SCORE_TIMER
-	#la s2, TEMPO_INICIAL_SCORE_TIMER
-	#sw a0, 0(s2)	# Inicializa: TEMPO_INICIAL_SCORE_TIMER = TEMPO_ATUAL	
+	la s2, TEMPO_INICIAL_SCORE_TIMER
+	sw a0, 0(s2)	# Inicializa: TEMPO_INICIAL_SCORE_TIMER = TEMPO_ATUAL			
 
 	# Inicializa TEMPO_INICIAL_MUSICA
 	la s2, TEMPO_INICIAL_MUSICA
-	sw a0, 0(s2)	# Inicializa: TEMPO_INICIAL_MUSICA = TEMPO_ATUAL		
+	li t0, 0
+	sw t0, 0(s2)	# Inicializa: TEMPO_INICIAL_MUSICA = TEMPO_ATUAL
 
 ######################
 # Teste
@@ -55,35 +56,40 @@ CONFIGURA_FASE_1:
 
 INICIO_GAME_LOOP_FASE_1:
 
-	#.include "REDUZ_TIMER.s"
+	.include "REDUZ_TIMER.s"
 
-	#.include "TECLADO_FASE_1.s"
+	.include "TECLADO_FASE_1.s"
 
-	# Pega tempo em TEMPO_INICIAL_MUSICA
-	la s2, TEMPO_INICIAL_MUSICA	# Pega endereco de TEMPO_INICIAL_MUSICA
-	lw s2, 0(s2)                    # Pega o conteudo de TEMPO_INICIAL_MUSICA
+	la t0, INDICE_NOTA					# t0 = &INDICE_NOTA
+	lw t1, 0(t0)						# t1 = conteudo INDICE_NOTA
+
+	# Garantia de primeiro funcionamento no loop 0
+	beqz t1, TOCA_NOTA_FASE_1
 
 	# Pega tempo atual
 	li a7, 30           # Chama a funcao TIME()
 	ecall               # Chama o Sistema operacional
 
-	# Pega o tempo passado desde o tempo inicial
-	sub t3, a0, s2			# t3 = TEMPO_ATUAL - TEMPO_INICIAL_TESTE
+	# Pega tempo em TEMPO_INICIAL_MUSICA
+	la s2, TEMPO_INICIAL_MUSICA		# Pega endereco de TEMPO_INICIAL_MUSICA
+	lw s2, 0(s2)                    # Pega o conteudo de TEMPO_INICIAL_MUSICA
 
-	la t0, INDICE_NOTA	# t0 = &INDICE_NOTA
-	lw t0, 0(t0)		# t0 = conteudo INDICE_NOTA
-	li t1, 8			# 8 bytes corresponde a uma Nota
-	mul t0, t0, t1		# t0 * 8 == numero de bytes a serem pulados ateh a nota atual
+	# Condicao para tocar nota
+	bge a0, s2, TOCA_NOTA_FASE_1
+	j FIM_TOCA_NOTA_FASE_1
 
+TOCA_NOTA_FASE_1:
+	# Conversao de indice de nota para Endereco da nota
+	# Calcula quantos bytes devem ser pulados
+	li t0, 8			# 8 bytes corresponde a uma Nota
+	mul t0, t0, t1		# t1 * 8 == numero de bytes a serem pulados ateh a nota atual
+
+	# Adiciona os bytes pulados ao endereco inicial da musica
 	la t4, NOTAS_MUSICA_FUNDO_FASE_1	# Pega endereco de NOTAS_MUSICA_FUNDO_FASE_1
 	add t4, t4, t0						# t4 = Endereco da nota atual
-	lw t2, 4(t4)						# t2 = Duracao da nota atual
 
-	blt t3, t2, PULA_TOCA_PROXIMA_NOTA # Se passou mais tempo que a duracao da nota, "TOCA_PROXIMA_NOTA"
-
-	# TOCA_PROXIMA_NOTA
 	# Atualiza valores no "reprodutor de som"
-	addi t4, t4, 8		# t4 = Endereco proxima nota
+	#addi t4, t4, 8		# t4 = Endereco proxima nota
 	lw a0, 0(t4)		# a0 = valor da proxima nota
 	lw a1, 4(t4)		# a1 = duracao da proxima nota
 
@@ -91,36 +97,33 @@ INICIO_GAME_LOOP_FASE_1:
 	li a7, 31			# Configura sistema para tocar a nota midi escolhida acima
 	ecall				# Chama o sistema operacional
 
-	# Atualiza TEMPO_INICIAL_MUSICA
+	# Atualiza Nota
+	lw t3, 4(t4)		# Pega duracao da musica
+	# Pega Tempo atual
 	li a7, 30   # Chama a funcao TIME()
 	ecall       # Chama o Sistema operacional
-	
-	la s2, TEMPO_INICIAL_MUSICA    		# Pega endereco de TEMPO_INICIAL_MUSICA
-	sw a0, 0(s2)                        # Atualiza o conteudo de TEMPO_INICIAL_MUSICA
 
-	# Atualiza variaveis relacionadas a nota
-	la t0, INDICE_NOTA					# t0 = &INDICE_NOTA
-	lw t1, 0(t0)						# t1 = conteudo INDICE_NOTA
-	addi t1, t1, 1						# t1++	: Passa para a proxima nota
+	add t3, a0, t3	# Tempo_proxima_nota = Tempo_atual + Duracao_nota_atual
+
+	# Pega tempo em TEMPO_INICIAL_MUSICA
+	la s2, TEMPO_INICIAL_MUSICA		# Pega endereco de TEMPO_INICIAL_MUSICA
+	sw t3, 0(s2)                    # TEMPO_INICIAL_MUSICA = Tempo_proxima_nota
 
 	# Permite loop musical
+	addi t1, t1, 1						# t1++	: Passa para a proxima nota
 	la t2, TAMANHO_MUSICA				# Pega endereco do TAMANHO_MUSICA
 	lw t2, 0(t2)						# Pega conteudo do TAMANHO_MUSICA
 	rem t1, t1, t2						# t1 = t1 % t2 : INDICE_NOTA = INDICE_NOTA % TAMANHO_MUSICA : Gera o loop musical
+	la t0, INDICE_NOTA					# t0 = &INDICE_NOTA
 	sw t1, 0(t0)						# INDICE_NOTA = t1
-	bnez t1, INICIO_GAME_LOOP_FASE_1	# Se INDICE_NOTA
 
-# Ao tentar garantir que a primeira nota fosse tocada no loop 0, criei uma "nota falsa" (0, 0) na posicao 0
-# Ou seja, a musica de verdade soh existe da posicoes 1 em diante.
-# Por isso eh precisa a correcao abaixo
-	#addi t1, t1, 1		# Pula a nota inexistente
-	#sw t1, 0(t0)		# INDICE_NOTA = t1
 
-PULA_TOCA_PROXIMA_NOTA:
+FIM_TOCA_NOTA_FASE_1:
 	j INICIO_GAME_LOOP_FASE_1
 
 FIM_GAME_LOOP_FASE_1:
-
+	li a7, 10
+	ecall
 #CONFIGURA_FASE_2:
 #INICIO_GAME_LOOP_FASE_2:
 
